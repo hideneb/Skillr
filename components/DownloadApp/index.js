1378ddd7421f6ddf18bbbdecf673893225f64ba1
Marius Repede <wiadev@users.noreply.github.com>
2021-07-27 12:03:24 -0700

    Update DownloadApp component

--- components/DownloadApp/index.js
+++ components/DownloadApp/index.js
@@ -4,18 +4,27 @@ import styles from "./DownloadApp.module.css";
 
 import appstoreImage from "../../public/appstore-coming.png";
 
-const DownloadApp = () => (
-  <div className={styles.container}>
-    <Image src={appstoreImage} alt="App Store Coming Soon" />
-    <p className={styles.description}>
-      Sign up below to receive exclusive invite.
-    </p>
+const DownloadApp = () => {
+  const handleSubmit = () => {};
 
-    <div className={styles.inviteContainer}>
-      <input className={styles.input} placeholder="Enter email..." />
-      <button className={styles.button}>Get Invite</button>
+  return (
+    <div className={styles.container}>
+      <Image src={appstoreImage} alt="App Store Coming Soon" />
+      <p className={styles.description}>
+        Sign up below to receive exclusive invite.
+      </p>
+
+      <form className={styles.inviteContainer} onSubmit={handleSubmit}>
+        <input
+          type="email"
+          className={styles.input}
+          placeholder="Enter email..."
+          required
+        />
+        <button className={styles.button}>Get Invite</button>
+      </form>
     </div>
-  </div>
-);
+  );
+};
 
 export default DownloadApp;
