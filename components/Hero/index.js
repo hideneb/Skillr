d7a8a24b2dad2f1c14e70487440d077011390c28
Eddy Fabery <eddyfabery@gmail.com>
2021-08-04 10:19:44 -0700

    Raw img tags without optimization

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React from "react";
 import styles from "./Hero.module.css";
 import screensImage from "../../public/hero-screens.png";
@@ -7,14 +6,14 @@ import screensMobileImage from "../../public/hero-mobile.png";
 const Hero = () => (
   <div className={styles.container}>
     <div>
-      <Image src="/logo.svg" width={73} height={102} alt="Skillr Logo" />
+      <img src="/logo.svg" width={73} height={102} alt="Skillr Logo" />
       <h1 className={styles.title}>Professional Help At Your Fingertips</h1>
       <div className={styles.screens}>
         <div className={styles.screensDesktop}>
-          <Image src={screensImage} layout="fixed" alt="Skillr screens" />
+          <img src={screensImage.src} layout="fixed" alt="Skillr screens" />
         </div>
         <div className={styles.screensMobile}>
-          <Image src={screensMobileImage} layout="fixed" alt="Skillr screens" />
+          <img src={screensMobileImage.src} layout="fixed" alt="Skillr screens" />
         </div>
       </div>
     </div>
--- pages/success/index.js
+++ pages/success/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import styles from "../../styles/SuccessPage.module.css";
 import logoTextBlackIcon from "../../public/logo-black-text.svg";
 
@@ -7,7 +6,7 @@ const SuccessPage = () => {
   return (
     <div className={styles.SuccessPage}>
       <div>
-        <Image src={logoTextBlackIcon} alt="Logo" />
+        <img src={logoTextBlackIcon.src} alt="Logo" />
         <h1 className={styles.Heading}>You&apos;re on the list.</h1>
         <p>Check your email to confirm in a few minutes.</p>
       </div>
