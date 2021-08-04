6984d8c0d88f5c470ec5a7b52163a929c8eac970
Eddy Fabery <eddyfabery@gmail.com>
2021-08-04 09:02:12 -0700

    Revert to 894186a1c106872ff8b0530d542e86dc50f72471

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
 }
-
-module.exports = withPlugins([
-  [optimizedImages, {
-    /* config for next-optimized-images */
-  }],
-
-  // your other plugins here
-], nextConfig);
