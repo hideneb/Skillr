a6f1518cff91e2da4a56c44bcbeed5ed7f06ca9f
Marius Repede <wiadev@users.noreply.github.com>
2021-09-15 13:30:59 -0400

    WEB-58: implement popup window

---
+++ components/SuccessModal/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./Modal.module.css"
+import IconClose from "../../public/icons/icon-close.svg"
+import Lottie from "react-lottie";
+import logo_animation_data from "../../public/lottie-logo/skillr_logo.json"
+
+export default function SuccessModal({ show, onClose }) {
+
+  const handleCloseClick = (e) => {
+    e.preventDefault()
+    onClose()
+  }
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: logo_animation_data,logo_animation_data,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  if (!show) return null;
+
+  return (
+    <div className={styles.modalWrapper}>
+      <div className={styles.modal}>
+        <a href="#" onClick={handleCloseClick} className={styles.closeIcon}>
+          <img src={IconClose.src} alt="Modal Close" />
+        </a>
+        <div className={styles.modalContent}>
+          <Lottie options={defaultOptions} width={240} height={270} />
+          <div className={styles.contentBox}>
+            <h2 className={styles.listTitle}>You're on the list!</h2>
+            <p className={styles.listDescription}>Check your email to confirm in a few minutes.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
