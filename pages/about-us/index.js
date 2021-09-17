7e008d9a3ca4c3dd291c8a172a78873e82a38612
Marius Repede <wiadev@users.noreply.github.com>
2021-09-17 13:38:36 -0400

    WEB-76: update page title

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import AboutUs from "../../components/AboutUs";
+import Head from "next/head";
 
 export default function AboutUsPage() {
-  return <AboutUs />;
+  return (
+    <div>
+      <Head>
+        <title>About Us | Skillr</title>
+        <meta name="description" content="Skillr" />
+        <link
+          rel="preload"
+          href="/fonts/RedHatDisplay/RedHatDisplay-Regular.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <AboutUs />
+    </div>
+  );
 }
