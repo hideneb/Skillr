044cdb24967b121a72d8eec72a247d3af54bf056
Marius Repede <wiadev@users.noreply.github.com>
2021-10-05 00:56:09 -0400

    WEB-69: About us page- With images layout

--- components/Bio/index.js
+++ components/Bio/index.js
@@ -2,26 +2,49 @@ import React from "react";
 import styles from "./Bio.module.css";
 import classNames from "classnames";
 
-export default function Bio({ size, isHover, img, name, skill, body }) {
+export default function Bio({
+  size,
+  hoverImg,
+  isHover,
+  img,
+  name,
+  skill,
+  body,
+}) {
+  console.log(hoverImg !== undefined);
   return (
     <div
       className={classNames(
         styles.bioContainer,
-        size == "big" ? styles.bigBio : styles.smallBio,
+        size == "big"
+          ? hoverImg !== undefined
+            ? classNames(styles.bigBioHover, styles.bigBio)
+            : styles.bigBio
+          : styles.smallBio,
         isHover && styles.isHover
       )}
     >
-      <div className={classNames(styles.relative, styles.bioImg)}>
-        <img src={img} alt={name} />
-        <div className={classNames(styles.absolute, styles.bioDetail)}>
-          <p className={styles.bioName}>{name}</p>
-          <p className={styles.bioSkill}>{skill}</p>
+      <div>
+        <div className={classNames(styles.relative, styles.bioImg)}>
+          <img src={img} alt={name} className={styles.originImg} />
+          {hoverImg !== "" && (
+            <img
+              src={hoverImg}
+              alt={name}
+              className={classNames(hoverImg !== "" && styles.hoverImg)}
+            />
+          )}
+
+          <div className={classNames(styles.absolute, styles.bioDetail)}>
+            <p className={styles.bioName}>{name}</p>
+            <p className={styles.bioSkill}>{skill}</p>
+          </div>
         </div>
-      </div>
-      <div
-        className={classNames(styles.bioContent, isHover && styles.absolute)}
-      >
-        <p className={styles.bioBody}>{body}</p>
+        <div
+          className={classNames(styles.bioContent, isHover && styles.absolute)}
+        >
+          <p className={styles.bioBody}>{body}</p>
+        </div>
       </div>
     </div>
   );
