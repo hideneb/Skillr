8d7f9e20789bb336dc498abb5bccd84feb80312e
Marius Repede <wiadev@users.noreply.github.com>
2021-10-12 17:52:36 -0400

    WEB-69: About us page- With images layout

---
+++ components/About/BioData/index.js
@@ -0,0 +1,244 @@
+import TonyBuckner from "../../../public/bio/TonyBuckner.png";
+import BrandonBaptiste from "../../../public/bio/BrandonBaptiste.png";
+import MaxSannikov from "../../../public/bio/MaxSannikov.png";
+import BlakeKeng from "../../../public/bio/BlakeKeng.png";
+import DimaZaiats from "../../../public/bio/DimaZaiats.png";
+import DemitriyLevy from "../../../public/bio/DemitriyLevy.png";
+import EricSchultz from "../../../public/bio/EricSchultz.png";
+import PeterShapiro from "../../../public/bio/PeterShapiro.png";
+import RosannaSpucces from "../../../public/bio/RosannaSpucces.png";
+import SushaUshakova from "../../../public/bio/SushaUshakova.png";
+import AlessandraBulow from "../../../public/bio/AlessandraBulow.png";
+import GigiGencturk from "../../../public/bio/GigiGencturk.png";
+
+import ElizabethHarrow from "../../../public/bio/ElizabethHarrow.png";
+import AddisonMcDowell from "../../../public/bio/AddisonMcDowell.png";
+import JordanMauro from "../../../public/bio/JordanMauro.png";
+import JCPagan from "../../../public/bio/JCPagan.png";
+import BillGlaser from "../../../public/bio/BillGlaser.png";
+import BrettModel from "../../../public/bio/BrettModel.png";
+import JasonGanfer from "../../../public/bio/JasonGanfer.png";
+import RyanMcHenry from "../../../public/bio/RyanMcHenry.png";
+
+import CasselShapiro from "../../../public/bio/CasselShapiro.png";
+import WebsterRoss from "../../../public/bio/WebsterRoss.png";
+import StevenMiller from "../../../public/bio/StevenMiller.png";
+import CasselShapiroHover from "../../../public/bio/CasselShapiro-hover.png";
+import WebsterRossHover from "../../../public/bio/WebsterRoss-hover.png";
+import StevenMillerHover from "../../../public/bio/StevenMiller-hover.png";
+
+import DavidIshag from "../../../public/bio/DavidIshag.png";
+import VictorAnthony from "../../../public/bio/VictorAnthony.png";
+
+export const dt_crew = [
+  {
+    size:"big",
+    img:CasselShapiro.src,
+    hoverImg:CasselShapiroHover.src,
+    name:"Cassel Shapiro",
+    skill:"Chief Executive and Board Leader",
+    body:"Senior Managing Director of SternAegis Ventures, Founder of Aegis Special Funds, Agritech Partners, Tech Insight Partners and Spring Targeted Opportunity Funds. Current Director DermaSensor, Inc.  and Outstanding Foods. Advisor to DarioHealth (NASDAQ: DRIO). Certified Developer in C#.Net, FINRA Principal, Investment Banking, Research Supervisor and Equities Market Making. I am devoted to executing our mission by assembling and leading a nimble organization. I love venture strategy and bringing big ideas to fruition.  ",
+  },
+  {
+    size:"big",
+    img:WebsterRoss.src,
+    hoverImg:WebsterRossHover.src,
+    name:"Webster Ross III",
+    skill:"Chief Technology",
+    body:"Co-founder and former CTO of venture-backed startups Zipdrug & Mixlab. I'm a veteran of New York City's startup community with over 15 years of experience in tech and a master's degree in Computer Engineering from Columbia University. As a serial entrepreneur, taking ventures from zero to one is my passion.",
+  },
+  {
+    size:"big",
+    img:StevenMiller.src,
+    hoverImg:StevenMillerHover.src,
+    name:"Steven Miller",
+    skill:"Chief Marketing",
+    body:"Former VP of Marketing at Kenzie Academy from Southern New Hampshire University, rebuilt their efforts and lead them through acquisition. Previously Head of Partnerships and Biz Dev for Shapr, leading their North America and UK expansion. I'm a marketing leader advising orgs across fintech, martech, and consumer technologies to help guide and build growth strategies. Organize teams that are nimble and thrive in start-up atmospheres.",
+  },
+]
+
+export const dt_crew2 = [
+  {
+    size: "small",
+    img: TonyBuckner.src,
+    name: "Tony Buckner",
+    skill: "Brand and Influencer Partnerships",
+    body: "Former First Data iOS Production Engineer, Giaia Wearables iOS Developer",
+  },
+  {
+    size: "small",
+    img: BrandonBaptiste.src,
+    name: "Brandon Baptiste",
+    skill: "Senior Front-End Developer",
+    body: "15 years executing global corporate branding partnerships and experiential marketing projects",
+  },
+  {
+    size: "small",
+    img: MaxSannikov.src,
+    name: "Max Sannikov",
+    skill: "Senior Back-End Developer",
+    body: "Full-Former Google Borg Software Engineer. Full Stack Engineer, passionate about technical challenges, product management and finance",
+  },
+  {
+    size: "small",
+    img: BlakeKeng.src,
+    name: "Blake Keng",
+    skill: "Director of UI/UX Design",
+    body: "UX Designer with extensive experience in web and mobile product development, focused on creating intuitive and visually engaging interface solutions. Passionate about innovation and quality.",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: DimaZaiats.src,
+    name: "Dima Zaiats",
+    skill: "Head UI/UX Design",
+    body: "UX Designer with extensive experience in web and mobile product development, focused on creating intuitive and visually engaging interface solutions. Passionate about innovation and quality.",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: DemitriyLevy.src,
+    name: "Demitriy Levy",
+    skill: "Back-End Developer",
+    body: "Current Backend Engineer for Skillr. Creator of gRPC Framework for Node and an Entrepreneur for Lead-Generation in Car and Real-Estate Business",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: EricSchultz.src,
+    name: "Eric Schultz",
+    skill: "Fractional CFO",
+    body: "Managing Member, Reliant Fund Services, Owner, EAS Accounting Services",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: PeterShapiro.src,
+    name: "Peter Shapiro",
+    skill: "Operations and Administration",
+    body: "Former data analyst for Democratic Party ",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: RosannaSpucces.src,
+    name: "Rosanna Spucces",
+    skill: "Senior UI/UX Designer",
+    body: "Three time published Graphic Designer and UX Designer with experience working with large scale brands such as: Star Wars, Live Nation, Space X, Blizzard, and Disney. ",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: SushaUshakova.src,
+    name: "Susha Ushakova",
+    skill: "Fitness and Wellness Coordinator",
+    body: "Highly skilled and committed fitness enthusiast with 8 years and 3000+ classes of experience.",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: AlessandraBulow.src,
+    name: "Alessandra Bulow",
+    skill: "Food and Beverage Expert",
+    body: "Food Editor NBCUniversal Media",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: GigiGencturk.src,
+    name: "Gigi Gencturk",
+    skill: "Office Manager",
+    body: "Currently Office Manager and Executive Admin, SternAegis Ventures",
+  },
+];
+
+export const dt_directors = [
+  {
+    size:"big",
+    img:CasselShapiro.src,
+    name:"Cassel Shapiro",
+    skill:"Chief Executive and Board Leader",
+    body:"Senior Managing Director of SternAegis Ventures, Founder of Aegis Special Funds, Agritech Partners, Tech Insight Partners and Spring Targeted Opportunity Funds. Current Director DermaSensor, Inc.  and Outstanding Foods. Advisor to DarioHealth (NASDAQ: DRIO). Certified Developer in C#.Net, FINRA Principal, Investment Banking, Research Supervisor and Equities Market Making. I am devoted to executing our mission by assembling and leading a nimble organization. I love venture strategy and bringing big ideas to fruition.  ",
+  },
+  {
+    size:"big",
+    img:DavidIshag.src,
+    name:"David Ishag",
+    skill:"Board Member ",
+    body:"David Ishag recently joined the Board of Zoomcar and Indian Mobility company with plans to expand throughout Asia and South America. Mr. Ishag has significant experience in the mobility space having served as Chairman of Karhoo, a ride-hailing and comparison app company which had ~250K cars on its platform, from 2014 to 2016. From December 2016 to February 2018, Mr. Ishag was also Special Advisor to Virgin Hyperloop One, a paradigm-shifting mobility company developing a super speed ground-level transportation system conceived by Elon Musk. Mr. Ishag also previously served as COO Europe for the leading internet incubator, Idealab, incubating companies such as Overture.com (sold to Yahoo for $1+B) and was involved in raising ~$1B in capital. Prior to that, he was COO and CFO of Capella Films, which produced films such Carlito’s Way. David has a Law Degree from the London School of Economics and an MBA from Harvard.",
+  },
+  {
+    size:"big",
+    img:VictorAnthony.src,
+    name:"Victor Anthony",
+    skill:"Board Member ",
+    body:"Media and Tech Investment Manager, Epoch Investment Partners Inc. Noted tech pundit on CNBC, Bloomberg, Wall Street Journal, Barron’s, NYTimes.",
+  }
+]
+
+export const dt_advisors = [
+  {
+    size: "small",
+    isHover: true,
+    img: ElizabethHarrow.src,
+    name: "Elizabeth Harrow",
+    skill: "Advisor",
+    body: "Vice President, Multicultural Innovation Lab at Morgan Stanley",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: AddisonMcDowell.src,
+    name: "Addison McDowell",
+    skill: "Advisor",
+    body: "Managing Director, Fields Texas Ltd.",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: JordanMauro.src,
+    name: "Jordan Mauro",
+    skill: "Advisor",
+    body: "Manager, Strategy & M&A Tata Consultancy. Former Manager KPMG M&A",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: JCPagan.src,
+    name: "JC Pagan",
+    skill: "Advisor",
+    body: "Founding Partner & Executive Creative Director at Sunday Afternoon. Former Deutsch, Creative Director",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: BillGlaser.src,
+    name: "Bill Glaser",
+    skill: "Advisor",
+    body: "CEO Outstanding Foods, Former CEO uKarma Corp. ",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: BrettModel.src,
+    name: "Brett Model",
+    skill: "Advisor",
+    body: "Brett is a hospitality and regional expert in Asia's most challenging work environments. Former Director Hospitality MGM Grand, Six Senses Hotels, Golden Nugget Lake Charles and quality.",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: JasonGanfer.src,
+    name: "Jason Ganfer",
+    skill: "Advisor",
+    body: "Counsel at Ganfer Shore Leeds & Zauderer, LLP",
+  },
+  {
+    size: "small",
+    isHover: true,
+    img: RyanMcHenry.src,
+    name: "Ryan McHenry",
+    skill: "Advisor",
+    body: "Controller, Sio Gene Therapies. Former Technical Accounting Senior Manager, CareFusion",
+  },
+];
--- components/About/index.js
+++ components/About/index.js
@@ -9,201 +9,9 @@ import fingerImage from "../../public/screenshot-4.jpeg";
 import aboutTeam from "../../public/about-team.png";
 import DownloadApp from "../DownloadApp";
 import classNames from "classnames";
-import Link from "next/link";
 import Bio from "../Bio";
 
-import CasselShapiro from "../../public/bio/CasselShapiro.png";
-import WebsterRoss from "../../public/bio/WebsterRoss.png";
-import StevenMiller from "../../public/bio/StevenMiller.png";
-import CasselShapiroHover from "../../public/bio/CasselShapiro-hover.png";
-import WebsterRossHover from "../../public/bio/WebsterRoss-hover.png";
-import StevenMillerHover from "../../public/bio/StevenMiller-hover.png";
-
-import TonyBuckner from "../../public/bio/TonyBuckner.png";
-import BrandonBaptiste from "../../public/bio/BrandonBaptiste.png";
-import MaxSannikov from "../../public/bio/MaxSannikov.png";
-import BlakeKeng from "../../public/bio/BlakeKeng.png";
-import DimaZaiats from "../../public/bio/DimaZaiats.png";
-import DemitriyLevy from "../../public/bio/DemitriyLevy.png";
-import EricSchultz from "../../public/bio/EricSchultz.png";
-import PeterShapiro from "../../public/bio/PeterShapiro.png";
-import RosannaSpucces from "../../public/bio/RosannaSpucces.png";
-import SushaUshakova from "../../public/bio/SushaUshakova.png";
-import AlessandraBulow from "../../public/bio/AlessandraBulow.png";
-import GigiGencturk from "../../public/bio/GigiGencturk.png";
-
-import DavidIshag from "../../public/bio/DavidIshag.png";
-
-import ElizabethHarrow from "../../public/bio/ElizabethHarrow.png";
-import AddisonMcDowell from "../../public/bio/AddisonMcDowell.png";
-import JordanMauro from "../../public/bio/JordanMauro.png";
-import JCPagan from "../../public/bio/JCPagan.png";
-import BillGlaser from "../../public/bio/BillGlaser.png";
-import BrettModel from "../../public/bio/BrettModel.png";
-import JasonGanfer from "../../public/bio/JasonGanfer.png";
-import RyanMcHenry from "../../public/bio/RyanMcHenry.png";
-
-const dt_crew = [
-  {
-    size: "small",
-    img: TonyBuckner.src,
-    name: "Tony Buckner",
-    skill: "Brand and Influencer Partnerships",
-    body: "Former First Data iOS Production Engineer, Giaia Wearables iOS Developer",
-  },
-  {
-    size: "small",
-    img: BrandonBaptiste.src,
-    name: "Brandon Baptiste",
-    skill: "Senior Front-End Developer",
-    body: "15 years executing global corporate branding partnerships and experiential marketing projects",
-  },
-  {
-    size: "small",
-    img: MaxSannikov.src,
-    name: "Max Sannikov",
-    skill: "Senior Back-End Developer",
-    body: "Full-Former Google Borg Software Engineer. Full Stack Engineer, passionate about technical challenges, product management and finance",
-  },
-  {
-    size: "small",
-    img: BlakeKeng.src,
-    name: "Blake Keng",
-    skill: "Director of UI/UX Design",
-    body: "UX Designer with extensive experience in web and mobile product development, focused on creating intuitive and visually engaging interface solutions. Passionate about innovation and quality.",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: DimaZaiats.src,
-    name: "Dima Zaiats",
-    skill: "Head UI/UX Design",
-    body: "UX Designer with extensive experience in web and mobile product development, focused on creating intuitive and visually engaging interface solutions. Passionate about innovation and quality.",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: DemitriyLevy.src,
-    name: "Demitriy Levy",
-    skill: "Back-End Developer",
-    body: "Current Backend Engineer for Skillr. Creator of gRPC Framework for Node and an Entrepreneur for Lead-Generation in Car and Real-Estate Business",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: EricSchultz.src,
-    name: "Eric Schultz",
-    skill: "Fractional CFO",
-    body: "Managing Member, Reliant Fund Services, Owner, EAS Accounting Services",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: PeterShapiro.src,
-    name: "Peter Shapiro",
-    skill: "Operations and Administration",
-    body: "Former data analyst for Democratic Party ",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: RosannaSpucces.src,
-    name: "Rosanna Spucces",
-    skill: "Senior UX/UI Designer",
-    body: "UX and Graphic Designer with experience working with large scale brands such as: Star Wars, Live Nation, Space X, and Disney. ",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: SushaUshakova.src,
-    name: "Susha Ushakova",
-    skill: "Fitness and Wellness Coordinator",
-    body: "Bio text here",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: AlessandraBulow.src,
-    name: "Alessandra Bulow",
-    skill: "Food and Beverage Expert",
-    body: "Food Editor NBCUniversal Media",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: GigiGencturk.src,
-    name: "Gigi Gencturk",
-    skill: "Office Manager",
-    body: "Currently Office Manager and Executive Admin, SternAegis Ventures",
-  },
-];
-
-const dt_advisors = [
-  {
-    size: "small",
-    isHover: true,
-    img: ElizabethHarrow.src,
-    name: "Elizabeth Harrow",
-    skill: "Advisor",
-    body: "Vice President, Multicultural Innovation Lab at Morgan Stanley",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: AddisonMcDowell.src,
-    name: "Addison McDowell",
-    skill: "Advisor",
-    body: "Managing Director, Fields Texas Ltd.",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: JordanMauro.src,
-    name: "Jordan Mauro",
-    skill: "Advisor",
-    body: "Manager, Strategy & M&A Tata Consultancy. Former Manager KPMG M&A",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: JCPagan.src,
-    name: "JC Pagan",
-    skill: "Advisor",
-    body: "Founding Partner & Executive Creative Director at Sunday Afternoon. Former Deutsch, Creative Director",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: BillGlaser.src,
-    name: "Bill Glaser",
-    skill: "Advisor",
-    body: "CEO Outstanding Foods, Former CEO uKarma Corp. ",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: BrettModel.src,
-    name: "Brett Model",
-    skill: "Advisor",
-    body: "Brett is a hospitality and regional expert in Asia's most challenging work environments. Former Director Hospitality MGM Grand, Six Senses Hotels, Golden Nugget Lake Charles and quality.",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: JasonGanfer.src,
-    name: "Jason Ganfer",
-    skill: "Advisor",
-    body: "Counsel at Ganfer Shore Leeds & Zauderer, LLP",
-  },
-  {
-    size: "small",
-    isHover: true,
-    img: RyanMcHenry.src,
-    name: "Ryan McHenry",
-    skill: "Advisor",
-    body: "Controller, Sio Gene Therapies. Former Technical Accounting Senior Manager, CareFusion",
-  },
-];
+import { dt_crew2, dt_advisors, dt_directors, dt_crew } from "./BioData";
 
 export default function About() {
   const [isActive, setActive] = useState(1);
@@ -316,35 +124,23 @@ export default function About() {
             style={{ display: isActive == 1 ? "block" : "none" }}
           >
             <div className={styles.bioWrapper}>
-              <Bio
-                size="big"
-                img={CasselShapiro.src}
-                hoverImg={CasselShapiroHover.src}
-                name="Cassel Shapiro"
-                skill="Chief Executive and Board Leader"
-                body="Senior Managing Director of SternAegis Ventures, Founder of Aegis Special Funds, Agritech Partners, Tech Insight Partners and Spring Targeted Opportunity Funds. Current Director DermaSensor, Inc.  and Outstanding Foods. Advisor to DarioHealth (NASDAQ: DRIO). Certified Developer in C#.Net, FINRA Principal, Investment Banking, Research Supervisor and Equities Market Making. I am devoted to executing our mission by assembling and leading a nimble organization. I love venture strategy and bringing big ideas to fruition.  "
-              />
-              <Bio
-                size="big"
-                img={WebsterRoss.src}
-                hoverImg={WebsterRossHover.src}
-                name="Webster Ross III"
-                skill="Chief Technology"
-                body="Senior Managing Director of SternAegis Ventures, Founder of Aegis Special Funds, Agritech Partners, Tech Insight Partners and Spring Targeted Opportunity Funds. Current Director DermaSensor, Inc.  and Outstanding Foods. Advisor to DarioHealth (NASDAQ: DRIO). Certified Developer in C#.Net, FINRA Principal, Investment Banking, Research Supervisor and Equities Market Making. I am devoted to executing our mission by assembling and leading a nimble organization. I love venture strategy and bringing big ideas to fruition.  "
-              />
-              <Bio
-                size="big"
-                img={StevenMiller.src}
-                hoverImg={StevenMillerHover.src}
-                name="Steven Miller"
-                skill="Chief Marketing"
-                body="Former AVP Marketing of Kenzie Academy, Head of Biz Dev at Shapr. With a focus on rational yet innovative ways to drive user acquisition and revenue growth, my ability to recognize and act on opportunities that lower cost and increase performance drive the success I deliver for organizations. Growing teams that are nimble and determined is my specialty"
-              />
+              {
+                dt_crew.map( crew => (
+                  <Bio
+                    size={crew.size}
+                    img={crew.img}
+                    hoverImg={crew.hoverImg}
+                    name={crew.name}
+                    skill={crew.skill}
+                    body={crew.body}
+                  />
+                ) )
+              }
             </div>
 
             <div className={classNames(styles.bioWrapper, styles.ctMargin)}>
               <div className={styles.flexContainer}>
-                {dt_crew.map((crew, index) => (
+                {dt_crew2.map((crew, index) => (
                   <Bio
                     size={crew.size}
                     isHover={crew.isHover}
@@ -367,20 +163,17 @@ export default function About() {
               Board of Directors
             </h3> */}
             <div className={styles.bioWrapper}>
-              <Bio
-                size="big"
-                img={DavidIshag.src}
-                name="David Ishag"
-                skill="Board Member "
-                body="Mr. Ishag is a current director of Zoomcar Holdings. He has leadership roles within Karhoo, Virgin Hyperloop One, Idealab, Capella Films"
-              />
-              <Bio
-                size="big"
-                img={CasselShapiro.src}
-                name="Cassel Shapiro"
-                skill="Chief Executive and Board Leader"
-                body="Senior Managing Director of SternAegis Ventures, Founder of Aegis Special Funds, Agritech Partners, Tech Insight Partners and Spring Targeted Opportunity Funds. Current Director DermaSensor, Inc.  and Outstanding Foods. Advisor to DarioHealth (NASDAQ: DRIO). Certified Developer in C#.Net, FINRA Principal, Investment Banking, Research Supervisor and Equities Market Making. I am devoted to executing our mission by assembling and leading a nimble organization. I love venture strategy and bringing big ideas to fruition.  "
-              />
+              {
+                dt_directors.map( director => (
+                  <Bio
+                    size={director.size}
+                    img={director.img}
+                    name={director.name}
+                    skill={director.skill}
+                    body={director.body}
+                  />
+                ))
+              }
             </div>
           </div>
 
