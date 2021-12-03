2450f9a6a34c81f2c98c21ce9f6ceb1fea8cf7fb
Rajiev Timal <rtimal@gmail.com>
2021-12-03 07:26:18 -0500

    [WEB-96] Asynchronously load Zendesk widget to prevent next.js error (#108)
    * Remove previous script usage
    
    * fix import

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import { polyfill } from "smoothscroll-polyfill";
 import Navbar from "../components/Navbar";
 import { useRouter } from "next/router";
+import Script from 'next/script'
+
 
 function MyApp({ Component, pageProps }) {
   // polyfill();
@@ -10,6 +12,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <div>
+      <Script id="ze-snippet" strategy="afterInteractive" src="https://static.zdassets.com/ekr/snippet.js?key=f367139b-5b83-46c5-91ad-95306b7da40b"></Script>
       <Navbar page={router.pathname.slice(1)} />
       <Component {...pageProps} />
     </div>
