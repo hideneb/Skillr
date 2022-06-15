a6ea0d56b3dde4cf631c90c38faa317b80bc9721
chrisgraves <cgraves@gmail.com>
2022-06-15 11:42:20 -0400

    Fix build (#123)
    * Fix build
    
    * Fix runtime error

--- components/UI/ProfileVideos/ProfileVideos.tsx
+++ components/UI/ProfileVideos/ProfileVideos.tsx
@@ -1,7 +1,6 @@
 import "react-modal-video/css/modal-video.min.css";
-// @ts-ignore: no types available for react-modal-video
-const ModalVideo = dynamic(() => import("react-modal-video"), {
-	ssr: false,
+const ModalVideo: any = dynamic(() => import("react-modal-video"), {
+  ssr: false,
 });
 import styles from "./ProfileVideos.module.css";
 import { SkillrImageDto } from "../../../lib/types/skillr";
@@ -11,50 +10,54 @@ import { useState } from "react";
 import dynamic from "next/dynamic";
 
 type ProfileVideosProps = {
-	videos: SkillrImageDto[];
+  videos: SkillrImageDto[];
 };
 
 const ProfileVideos: React.FC<ProfileVideosProps> = ({ videos }) => {
-	const [videoOpen, setVideoOpen] = useState(false);
-	const [videoSource, setVideoSource] = useState("");
+  const [videoOpen, setVideoOpen] = useState(false);
+  const [videoSource, setVideoSource] = useState("");
 
-	const openVideo = (src: string) => {
-		setVideoSource(src);
-		setVideoOpen(true);
-	};
+  const openVideo = (src: string) => {
+    setVideoSource(src);
+    setVideoOpen(true);
+  };
 
-	const closeVideo = () => {
-		setVideoSource("");
-		setVideoOpen(false);
-	};
+  const closeVideo = () => {
+    setVideoSource("");
+    setVideoOpen(false);
+  };
 
-	return (
-		<>
-			<div
-				className={classNames(styles.profileVideos, {
-					[styles.singleVideoLayout]: videos.length === 1,
-				})}
-			>
-				{videos.map((v) => {
-					return (
-						<div key={v.id} className={styles.result} onClick={() => openVideo(v.video)}>
-							<PlayIcon className={styles.play} />
-							<img className={styles.video} src={v.image} alt="" />
-						</div>
-					);
-				})}
-			</div>
-			{typeof window !== "undefined" && (
-				<ModalVideo
-					channel="custom"
-					autoplay
-					url={videoSource}
-					isOpen={videoOpen}
-					onClose={() => closeVideo()}
-				/>
-			)}
-		</>
-	);
+  return (
+    <>
+      <div
+        className={classNames(styles.profileVideos, {
+          [styles.singleVideoLayout]: videos.length === 1,
+        })}
+      >
+        {videos.map((v) => {
+          return (
+            <div
+              key={v.id}
+              className={styles.result}
+              onClick={() => openVideo(v.video)}
+            >
+              <PlayIcon className={styles.play} />
+              <img className={styles.video} src={v.image} alt="" />
+            </div>
+          );
+        })}
+      </div>
+      {typeof window !== "undefined" && (
+        <ModalVideo
+          channel="custom"
+          autoplay={true}
+          url={videoSource}
+          isOpen={videoOpen}
+          onClose={() => closeVideo()}
+        />
+      )}
+    </>
+  );
 };
 
 export default ProfileVideos;
--- stories/BookSessionCard.stories.tsx
+++ stories/BookSessionCard.stories.tsx
@@ -4,38 +4,20 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import ProfileCard from "../components/UI/ProfileCard";
 
 export default {
-	title: "ProfileCard",
-	component: ProfileCard,
-	argTypes: {},
+  title: "ProfileCard",
+  component: ProfileCard,
+  argTypes: {},
 } as ComponentMeta<typeof ProfileCard>;
 
-const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />;
+const Template: ComponentStory<typeof ProfileCard> = (args) => (
+  <ProfileCard {...args} />
+);
 
 export const Primary = Template.bind({});
 
 Primary.args = {
-	name: "name",
-	description: "description",
-	imgSrc: "https://via.placeholder.com/150",
-	ratePerMinute: 100,
-	availability: {
-		day0Begin: "09:00:00",
-		day0End: "18:00:00",
-		day1Begin: "09:00:00",
-		day1End: "18:00:00",
-		day2Begin: "09:00:00",
-		day2End: "18:00:00",
-		day3Begin: "09:00:00",
-		day3End: "18:00:00",
-		day4Begin: "09:00:00",
-		day4End: "18:00:00",
-		day5Begin: "09:00:00",
-		day5End: "18:00:00",
-		day6Begin: "09:00:00",
-		day6End: "18:00:00",
-		timezoneOffset: 0,
-		skillrId: "1",
-		id: "1",
-		type: 0,
-	},
+  name: "name",
+  description: "description",
+  imgSrc: "https://via.placeholder.com/150",
+  ratePerMinute: 100,
 };
