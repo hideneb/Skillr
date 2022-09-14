48b981b0e6c46afe5d87710c638275b73f6e7793
Marius Repede <wiadev@users.noreply.github.com>
2022-09-14 13:24:04 -0700

    feat: update cloudinary images link with scale (#193)

--- components/UI/Onboarding/SkillMenuItem/SkillMenuItem.tsx
+++ components/UI/Onboarding/SkillMenuItem/SkillMenuItem.tsx
@@ -21,7 +21,7 @@ const SkillMenuItem: FC<Props> = ({ skill, category, setSkill }: Props) => {
             />
             <MenuItem onClick={() => setSkill(skill)} style={{ paddingLeft: 16, paddingRight: 16 }}>
                 <div className="flex py-1 ml-2 space-x-2 items-center">
-                    <img className="w-6 h-6 rounded-full" src={skill.lightIcon} alt={skill.lightIconFilename} />
+                    <img className="w-6 h-6 rounded-full" src={skill.lightIcon.replace('/upload/', '/upload/c_fill,w_100/')} alt={skill.lightIconFilename} />
                     <div className="space-y-0.5">
                         {category && <p className="text-xs text-gray-400">{category}</p>}
                         <p className="text-sm">{skill.name}</p>
